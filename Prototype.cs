22d1564e37a2f7dfd0b6fe4bae6e15efb6cb0676
Rumuantsev <117656273+Rumuantsev@users.noreply.github.com>
2023-03-13 12:20:16 +0700

    Add Lab3
    Add Lab3

---
+++ Prototype.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class PrototypeSquareMatrix : SquareMatrix, ICloneable
+    {
+        public PrototypeSquareMatrix(int Size) : base(Size) { }
+        public PrototypeSquareMatrix() : base() { }
+
+        public object Clone()
+        {
+            PrototypeSquareMatrix Result = new PrototypeSquareMatrix(Size);
+
+            Result.Size = this.Size;
+            Result.Matrix = this.Matrix;
+
+            return Result;
+        }
+    }
+}
